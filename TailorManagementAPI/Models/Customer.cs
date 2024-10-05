3e2b4fb18f2b5cd8e0d55e29abc022fcf42d8ef5
rakeshgohil <92530820+rakeshgohil@users.noreply.github.com>
2024-10-05 20:25:50 +1000

    Created API project along with Entity Core.

---
+++ TailorManagementAPI/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TailorManagementAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MaxLength(15)]
+        public string Phone { get; set; }
+
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
